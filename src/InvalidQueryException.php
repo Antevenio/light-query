76f293d3e2b58f36167f7219dbd37734fcb7cf77
pinfake <pinfake@hotmail.com>
2016-05-27 14:30:17 +0200

    Now depends on Antevenio/query-weigth

--- src/InvalidQueryException.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace LightQuery;
-
-class InvalidQueryException extends \Exception
-{
-}
--- src/LightQuery.php
+++ src/LightQuery.php
@@ -1,48 +1,34 @@
 <?php
 
 namespace LightQuery;
-
+use QueryWeight\QueryWeight;
 use PDO;
 
 class LightQuery
 {
+    /**
+     * @var PDO
+     */
     protected $pdo;
+    /**
+     * @var QueryWeight
+     */
+    protected $queryWeight;
     protected $maxRowsToCompute;
 
-    public function __construct( \PDO $pdo, $maxRowsToCompute )
+    public function __construct( PDO $pdo, QueryWeight $queryWeight, $maxRowsToCompute )
     {
         $this->pdo = $pdo;
+        $this->queryWeight = $queryWeight;
         $this->maxRowsToCompute = $maxRowsToCompute;
     }
 
     public function query($query) {
-        $rowsToCompute = $this->getQueryWeight($query);
+        $rowsToCompute = $this->queryWeight->getQueryWeight($query);
         if ($rowsToCompute > $this->maxRowsToCompute) {
             throw new HeavyQueryException();
         } else {
             return ($this->pdo->query($query, \PDO::FETCH_ASSOC));
         }
-    }
-
-    public function getQueryWeight($query)
-    {
-        if(!$this->isExplainable($query)) {
-            throw new InvalidQueryException();
-        }
-        $explain = "EXPLAIN " . $query;
-        $statement = $this->pdo->query( $explain );
-        $rows = $statement->fetchAll();
-
-        $total = 1;
-        foreach( $rows as $row )
-        {
-            $total *= $row['rows'];
-        }
-        return ($total);
-    }
-
-    protected function isExplainable($query)
-    {
-        return (preg_match('/^\s*SELECT\s+/i',$query));
     }
 }
